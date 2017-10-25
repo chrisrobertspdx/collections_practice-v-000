3bd6f039179bc26f6d7f57f39f84813897f45d8b
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-25 17:43:14 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,6 +2,13 @@ def sort_array_asc(arr)
   return arr.sort
 end
 
-def sort_array_asc(arr)
-  return arr.sort
+def sort_array_desc(arr)
+  return arr.sort{|a,b| 
+    if b>a
+      1
+    elsif b<a
+      -1
+    else
+      0
+  }
 end
